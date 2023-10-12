baa40d7696da3f918352e474bb039da09db85457
Nursultan <nursultanxabipnazarov@gmail.com>
2023-10-12 11:16:37 +0500

    header qosildi

---
+++ header.php
@@ -0,0 +1,47 @@
+<div class="header">
+      <div class="bg-color">
+        <header id="main-header">
+          <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="container">
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+                <a class="navbar-brand" href="#">Ba<span class="logo-dec">ker</span></a>
+              </div>
+              <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav navbar-right">
+                  <li class="active"><a href="#main-header">Home</a></li>
+                  <li class=""><a href="#feature">About</a></li>
+                  <li class=""><a href="#service">Services</a></li>
+                  <li class=""><a href="#portfolio">Portfolio</a></li>
+                  <li class=""><a href="#testimonial">Testimonial</a></li>
+                  <li class=""><a href="#blog">Blog</a></li>
+                  <li class=""><a href="#contact">Contact Us</a></li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </header>
+        <div class="wrapper">
+          <div class="container">
+            <div class="row">
+              <div class="banner-info text-center wow fadeIn delay-05s">
+                <h1 class="bnr-title">We are at ba<span>ker</span></h1>
+                <h2 class="bnr-sub-title">Starting a new journey!!</h2>
+                <p class="bnr-para">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip <br>ex ea commodo consequat.</p>
+                <div class="brn-btn">
+                  <a href="#" class="btn btn-download">Download now!</a>
+                  <a href="#" class="btn btn-more">Learn More</a>
+                </div>
+                <div class="overlay-detail">
+                  <a href="#feature"><i class="fa fa-angle-down"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
